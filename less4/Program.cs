104466ae7105db50827e6c9d546d7756bb9ee4e1
Seregaa10 <126242636+SeregaAtepalikhin@users.noreply.github.com>
2023-03-19 21:18:21 +0300

    commit

--- less4/Program.cs
+++ less4/Program.cs
@@ -1,7 +1,7 @@
 ﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 //3, 5 -> 243 (3⁵)
 //2, 4 -> 16
-
+/*
 Console.WriteLine("введите число A: ");
 int A = int.Parse(Console.ReadLine());
 Console.WriteLine("введите число B: ");
@@ -13,7 +13,7 @@ for (int i = 1; i <= B; i++)
   result = result * A;
 }
 Console.WriteLine(result);
-
+*/
 
 
 
