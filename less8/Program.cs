bfe6956df7b20e5b06e89f1fad16d24dfbafbe6f
Sergey Atepalikhin <vinyl51@ya.ru>
2023-04-07 01:38:07 +0300

    Less9

--- less8/Program.cs
+++ less8/Program.cs
@@ -10,8 +10,57 @@
 9 5 3 2
 8 4 4 2
 */
-
 /*
+int[,] massive = new int[3, 4];
+FillArrayRandom(massive);
+PrintArray(massive);
+SortNum(massive);
+Console.WriteLine();
+PrintArray(massive);
+
+void FillArrayRandom(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(1, 10);
+    }
+  }
+}
+void SortNum(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1); k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int fail = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+void PrintArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.WriteLine($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+  }
+}
+*/
+/*
+
 Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
 которая будет находить строку с наименьшей суммой элементов.
 Например, задан массив:
@@ -22,6 +71,72 @@
 Программа считает сумму элементов в каждой строке и 
 выдаёт номер строки с наименьшей суммой элементов: 1 строка
 */
+/*
+Console.WriteLine("Введите размер массива и диапазон случайных значений:");
+int row = InputNumbers("сколько строк: ");
+int column = InputNumbers("сколько столбцов: ");
+int range = InputNumbers("Введите диапазон: от 1 до ");
+
+int[,] array = new int[row, column];
+CreateArray(array);
+WriteArray(array);
+
+int minSumLine = 0;
+int sumLine = SumLineElements(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
+{
+  int tempSumLine = SumLineElements(array, i);
+  if (sumLine > tempSumLine)
+  {
+    sumLine = tempSumLine;
+    minSumLine = i;
+  }
+}
+
+Console.WriteLine($"\n{minSumLine+1} - строкa с наименьшей суммой ({sumLine}) элементов ");
+
+
+int SumLineElements(int[,] array, int i)
+{
+  int sumLine = array[i,0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sumLine += array[i,j];
+  }
+  return sumLine;
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(range);
+    }
+  }
+}
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
+*/
+
 
 /*
 Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
@@ -32,6 +147,76 @@
 18 20
 15 18
 */
+/*
+Console.WriteLine("Введите размеры матриц и диапазон случайных значений:");
+int m = InputNumbers("Введите число строк 1-й матрицы: ");
+int n = InputNumbers("Введите число столбцов 1-й матрицы (и строк 2-й): ");
+int p = InputNumbers("Введите число столбцов 2-й матрицы: ");
+int range = InputNumbers("Введите диапазон случайных чисел: от 1 до ");
+
+int[,] firstMartrix = new int[m, n];
+CreateArray(firstMartrix);
+Console.WriteLine($"Первая матрица:");
+WriteArray(firstMartrix);
+
+int[,] secomdMartrix = new int[n, p];
+CreateArray(secomdMartrix);
+Console.WriteLine($"Вторая матрица:");
+WriteArray(secomdMartrix);
+
+int[,] resultMatrix = new int[m, p];
+
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine($"Произведение первой и второй матриц:");
+WriteArray(resultMatrix);
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i, k] * secomdMartrix[k, j];
+      }
+      resultMatrix[i, j] = sum;
+    }
+  }
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(range);
+    }
+  }
+}
+
+void WriteArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
+*/
+
 
 /*
 Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
@@ -42,7 +227,76 @@
 27(0,0,1) 90(0,1,1)
 26(1,0,1) 55(1,1,1)
 */
+/*
 
+Console.WriteLine($"Введите размер массива X x Y x Z: ");
+int x = InputNumbers("Введите X: ");
+int y = InputNumbers("Введите Y: ");
+int z = InputNumbers("Введите Z: ");
+Console.WriteLine($"");
+
+int[,,] array3D = new int[x, y, z];
+CreateArray(array3D);
+WriteArray(array3D);
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+void WriteArray (int[,,] array3D)
+{
+  for (int i = 0; i < array3D.GetLength(0); i++)
+  {
+    for (int j = 0; j < array3D.GetLength(1); j++)
+    {
+      Console.Write($"X({i}) Y({j}) ");
+      for (int k = 0; k < array3D.GetLength(2); k++)
+      {
+        Console.Write( $"Z({k})={array3D[i,j,k]}; ");
+      }
+      Console.WriteLine();
+    }
+    Console.WriteLine();
+  }
+}
+void CreateArray(int[,,] array3D)
+{
+  int[] temp = new int[array3D.GetLength(0) * array3D.GetLength(1) * array3D.GetLength(2)];
+  int number;
+  for (int i = 0; i < temp.GetLength(0); i++)
+  {
+    temp[i] = new Random().Next(10, 100);
+    number = temp[i];
+    if (i >= 1)
+    {
+      for (int j = 0; j < i; j++)
+      {
+        while (temp[i] == temp[j])
+        {
+          temp[i] = new Random().Next(10, 100);
+          j = 0;
+          number = temp[i];
+        }
+        number = temp[i];
+      }
+    }
+  }
+  int count = 0;
+  for (int x = 0; x < array3D.GetLength(0); x++)
+  {
+    for (int y = 0; y < array3D.GetLength(1); y++)
+    {
+      for (int z = 0; z < array3D.GetLength(2); z++)
+      {
+        array3D[x, y, z] = temp[count];
+        count++;
+      }
+    }
+  }
+}
+*/
 /*
 Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
 Например, на выходе получается вот такой массив:
---
+++ less9/Program.cs
@@ -0,0 +1,92 @@
+﻿/*
+Задача 64: Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+/*
+Console.Write($"Задайте стартовое значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int M = 1;
+OutNumbers(n, M);
+
+void OutNumbers(int n, int M)
+{
+  if (M > n)
+  {
+    return;
+  }
+  else
+  {
+    OutNumbers(n, M + 1);
+    Console.Write(M + " ");
+  }
+}
+*/
+/*
+Задача 66: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+/*
+Console.Write($"Задайте значение N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Задайте значение M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+SumNM(N, M);
+
+void SumNM(int N, int M)
+{
+  Console.Write(SumNum(N - 1, M));
+}
+int SumNum(int N, int M)
+{
+  int result = N;
+  if (N == M)
+    return 0;
+  else
+  {
+    N++;
+    result = N + SumNum(M, N);
+    return result;
+  }
+}
+*/
+/*
+Задача 68: Напишите программу вычисления функции Аккермана 
+с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+/*
+Console.Write($"Задайте значение m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Задайте значение n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Akkerman(m, n);
+
+void Akkerman(int m, int n)
+{
+  Console.Write(AkkermanF(m, n));
+}
+
+int AkkermanF(int m, int n)
+{
+  if (m == 0)
+  {
+    return n + 1;
+  }
+  else if (n == 0 && m > 0)
+  {
+    return AkkermanF(m - 1, 1);
+  }
+  else
+  {
+    return (AkkermanF(m - 1, AkkermanF(m, n - 1)));
+  }
+}
+*/
